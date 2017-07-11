926cf0ecb471e2707ba296c8879a8592cbf04598
shaik jabbar <jabbars@Jabbars-MacBook-Pro.local>
2017-07-11 16:58:15 +0530

    Hello World in RatPack

---
+++ src/main/java/com/shaik/app/RatPackApplication.java
@@ -0,0 +1,17 @@
+package com.shaik.app;
+
+import ratpack.server.RatpackServer;
+
+/**
+ * Created by jabbars on 11/07/17.
+ */
+public class RatPackApplication {
+    public static void main(String[] args) throws Exception{
+
+        RatpackServer.start(serverSpec -> serverSpec
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("Hello, World!"))
+                )
+        );
+    }
+}
